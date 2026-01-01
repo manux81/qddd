ce8db5939197d3528034bc5074e821009465a9c8
Manuele Conti <manuele.conti@archimede-energia.com>
2026-01-01 14:50:45 +0100

    Enable High DPI scaling and HiDPI pixmap support on Qt >= 5.6
    Added QApplication and QCoreApplication attributes to enable DPI scaling and HiDPI pixmaps when running on Qt 5.6 or newer.

--- src/main.cpp
+++ src/main.cpp
@@ -35,6 +35,10 @@
 #include <QTimer>
 
 int main(int argc, char *argv[]) {
+#if QT_VERSION >= 0x050600
+	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling); // DPI support
+	QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps); //HiDPI pixmaps
+#endif
 	QApplication app(argc, argv);
 	app.setWindowIcon(QIcon(":icons/resources/icons/appicon.png"));
 	Q_INIT_RESOURCE(resources);
