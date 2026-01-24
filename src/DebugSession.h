f36a73eea984b7c162e4417d93c75ea935a6768f
Manuele Conti <manuele.conti@archimede-energia.com>
2026-01-24 17:39:14 +0100

    Set GDB_MI as default

--- src/DebugSession.h
+++ src/DebugSession.h
@@ -150,7 +150,7 @@ class DebugSession : public QObject {
 
   private:
 	QProcess m_proc;
-	Backend m_backend = Backend::LLDB_MI;
+	Backend m_backend = Backend::GDB_MI;
 	QString m_programPath;
 	QByteArray m_buffer;
 
